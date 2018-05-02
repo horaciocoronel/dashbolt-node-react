69ff6da12b2183174924d309d3be2330dd4a41a0
horaciocoronel <arielc@gmail.com>
2018-05-02 17:03:18 -0400

    Add dotenv to main App

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+
 
 const pictures = require('./routes/api/pictures');
 
