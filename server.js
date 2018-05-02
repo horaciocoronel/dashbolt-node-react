44f62965c4b50c4e963df0497289640530803ade
horaciocoronel <arielc@gmail.com>
2018-05-02 15:55:27 -0400

    Add conditional to serve static assets if in production.

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('hi');
-  }
-);
+// Serve static assets if in production 
+if(process.env.NODE_ENV=== 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	});
+}
 
 const port = process.env.PORT || 5000;
 
