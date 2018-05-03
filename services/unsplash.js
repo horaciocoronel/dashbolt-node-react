fe455726da16ad74484805da790d0ed6d5a7e79f
horaciocoronel <arielc@gmail.com>
2018-05-03 10:39:37 -0400

    Add unsplash service

---
+++ services/unsplash.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+const baseURL = "https://api.unsplash.com/photos/random";
+
+// Load Pictures model
+const Pictures = require('../models/Pictures');
+
+// DB Config
+const db = require('../config/keys').mongoURI;
+
+// Connect to mongoDB
+mongoose
+	.connect(db)
+	.then(() => console.log('MongoDB connected'))
+	.catch(err => console.log(err));
+
+
+export const getPictures = () => {
+  axios.get(`${baseURL}/?client_id=${keys.unsplashAccessKey}&featured=true&count=30&query=sky%20morning&orientation=landscape`)
+  .then(function (response) {
+    response.data.map(photo => {
+      const newPicture = new Pictures({
+        photoid: photo.id,
+        photourl: photo.urls.full,
+        username: photo.user.username,
+        fullname: photo.user.name,
+        likes: photo.likes
+      });
+      new Pictures(newPicture)
+        .save()
+        .then(profile => console.log(newPicture))
+        .then(function (response) {
+          mongoose.disconnect();
+        });
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+};
