657a8bf628cc9d780a94959493f380b38cbb0c4e
horaciocoronel <arielc@gmail.com>
2018-05-03 10:39:11 -0400

    Add Pictures model

---
+++ models/Pictures.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const PicturesSchema = new Schema({
+  photoid: {
+    type: String,
+    required: true
+  },
+    photourl: {
+      type: String,
+      required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  fullname: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: String,
+    required: false
+  }
+});
+
+module.exports = User = mongoose.model('pictures', PicturesSchema);
