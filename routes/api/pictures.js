45ec69ee2a033d3a2f8147bb653c2b9e608d8f5c
horaciocoronel <arielc@gmail.com>
2018-05-05 15:46:46 -0400

    Add GET /api/pictures/all endpoint

--- routes/api/pictures.js
+++ routes/api/pictures.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+// Load Pictures Model
+const Pictures = require('../../models/Pictures');
+
 // @route 	GET api/test
-// @desc 		Test users route
+// @desc 		Test  route
 // @access	Public
 router.get('/test', (req, res) => res.json({msg: 'Pictures works'}));
-router.get('/db', (req, res) => {res.json({msg: process.env.DB_HOST})});
+
+// @route 	GET api/pictures/all
+// @desc 		List all Pictures
+// @access	Public
+router.get('/all', (req, res) => {
+  Pictures.find()
+  .populate()
+  .then(pictures => {
+    if(!pictures) {
+      errors.pictures = 'There are no pictures'
+      return res.status(404).json(errors)
+    }
+    res.json(pictures)
+  })
+  .catch(err => 
+    res.status(404).json({pictures: 'There was a problem with the server. Please try again later'})
+  )
+});
 
 module.exports = router;
