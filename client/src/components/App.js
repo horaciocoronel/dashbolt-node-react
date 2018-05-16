4f55b39e9ff1862f973572a1f0e017b5e227567d
horaciocoronel <arielc@gmail.com>
2018-05-16 01:49:11 -0400

    Add Middle component to App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Middle from './layout/Middle';
 import './App.scss';
 
 export class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>My React App</h1>
+        <Middle />
       </div>
     )
   }
