143257211bceaebabbf921a9f06e1ccbb06a5c7d
horaciocoronel <arielc@gmail.com>
2018-05-16 01:49:32 -0400

    Add Middle layout and data for component!

--- client/src/components/layout/Middle.js
+++ client/src/components/layout/Middle.js
@@ -5,8 +5,20 @@ import Clock from 'react-live-clock';
 export class MiddleCenter extends Component {
   render() {
     return (
-      <div>
-        <Clock format={'HH:mm'} ticking={true}/>        
+      <div className="row">
+        <div className="middle">Feature</div>
+        <div className="middle center">
+          <div className="time">
+            <Clock format={'HH:mm'} ticking={true}/>
+          </div>
+          <div className="greeting">
+            Good morning, Horacio
+          </div>
+          <div className="focus">
+            focus
+          </div>
+        </div>
+        <div className="middle">Feature</div>
       </div>
     )
   }
