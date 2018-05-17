414715986909b9ce80c77d88d707e5cc4602df65
horaciocoronel <arielc@gmail.com>
2018-05-17 18:56:32 -0400

    Add fetchQuotes function to component did mount

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 import Middle from './layout/Middle';
+import {getQuotes} from '../utils/Quotes';
 import './App.scss';
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quoteText: '',
+      quoteAuthor: ''
+    }
+  }
+
+
+  fetchQuote() {
+    getQuotes()
+    .then(res => {
+      this.setState({quoteText: res.quoteText, quoteAuthor: res.quoteAuthor});
+    })
+  }
+
+  componentDidMount() {
+    this.fetchQuote()
+  }
+  
+
   render() {
     return (
       <div className="container">
         <h1>My React App</h1>
         <Middle />
+        <p>{this.state.quoteText}</p>
+        <p>{this.state.quoteAuthor}</p>
       </div>
     )
   }
