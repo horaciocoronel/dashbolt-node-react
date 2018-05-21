fe74b52bfbe0f601a5c551fc573d256af6eef07c
horaciocoronel <arielc@gmail.com>
2018-05-21 12:25:11 -0400

    Create quotes bottom component

---
+++ client/src/components/layout/Bottom.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {getQuotes} from '../../utils/Quotes';
+
+export class BottomCenter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quoteText: '',
+      quoteAuthor: ''
+    }
+  }
+
+  fetchQuote() {
+    getQuotes()
+    .then(res => {
+      this.setState({quoteText: res.quoteText, quoteAuthor: res.quoteAuthor});
+    })
+  }
+
+  componentDidMount() {
+    this.fetchQuote()
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="bottom">a</div>
+        <div className="bottom center">
+        <h3>{this.state.quoteText}</h3>
+        <h6>{this.state.quoteAuthor}</h6>
+        
+        </div>
+        <div className="bottom">a</div>
+    </div>
+    )
+  }
+}
+
+export default BottomCenter
