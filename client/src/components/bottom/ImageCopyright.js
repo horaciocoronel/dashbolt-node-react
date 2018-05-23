1d9a40a29eb30cf0cf0622e396df34438127bba0
horaciocoronel <arielc@gmail.com>
2018-05-23 10:28:17 -0400

    Add dynamic image data to component

--- client/src/components/bottom/ImageCopyright.js
+++ client/src/components/bottom/ImageCopyright.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 
 export class ImageCopyright extends Component {
   render() {
+    let imageData = this.props.imageData;
     return (
-      <div>
-        ImageCopyright
+      <div className="unsplash-copy">
+      Photo by <a href={`https://unsplash.com/photos/${imageData.photoid}`}>{imageData.fullname}</a> on <a href="https://unsplash.com/?utm_source=your_app_name&utm_medium=referral">Unsplash</a>
       </div>
     )
   }
 }
 
-export default ImageCopyright
+export default ImageCopyright;
