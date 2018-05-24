07a4da3f24ccb2b7e7678be0fde988754581fb89
horaciocoronel <arielc@gmail.com>
2018-05-24 13:26:25 -0400

    Add weather subComponent

---
+++ client/src/components/_Weather.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import WeatherIcon from 'react-icons-weather';
+
+const Weather = (props) => {
+  // console.log('props', props)
+    return (
+      <div className="top right">
+        <div className="weather-info">
+          <WeatherIcon
+            name='darksky'
+            iconId={props.weather.icon}
+            className='weather-icon'
+            fixedWidth={true}
+          >
+          <span className="weather-temperature">{Math.floor(props.weather.temperature)}°</span>
+          </WeatherIcon>
+        </div>
+        <div className="weather-city">
+          {props.city}
+        </div>
+      </div> 
+    )
+}
+
+export default Weather;
