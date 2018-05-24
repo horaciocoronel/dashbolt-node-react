36fb661c76883b199aa47596a56f216e0dd272fa
horaciocoronel <arielc@gmail.com>
2018-05-24 18:41:40 -0400

    Display message last time it run.

--- cron-jobs.js
+++ cron-jobs.js
@@ -1,4 +1,7 @@
 const cron = require('node-cron');
 const getPictures = require('./services/unsplash');
 
-cron.schedule('0 0,4,6,12,18,19 * * 0-6', getPictures);
+cron.schedule('0 0,4,6,12,18,19 * * 0-6', () => {
+  getPictures;
+  console.log(`Run at ${Date.now()}`)
+});
