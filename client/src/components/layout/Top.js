c0b2921eeb2521fed14c19908d36cd172d2d3518
horaciocoronel <arielc@gmail.com>
2018-05-24 17:52:05 -0400

    Refactor weather info to display only if data available.

--- client/src/components/layout/Top.js
+++ client/src/components/layout/Top.js
@@ -3,12 +3,21 @@ import React, { Component } from 'react'
 import Weather from '../_Weather';
 
 export class Top extends Component {
+
+  checkWeather() {
+    if (this.props.weather.temperature != '') {
+      return <Weather weather={this.props.weather} city={this.props.city} />
+    } else {
+      return <div className="top right"></div>
+    }
+  }
+
   render() {
     return (
       <div className="row">
       <div className="top left"></div>
       <div className="top center"></div>
-      <Weather weather={this.props.weather} city={this.props.city} />
+      {this.checkWeather()}
       </div>
     )
   }
