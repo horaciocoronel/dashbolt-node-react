9527a065dddbe39418ed3a146ff4ebda0abf88fb
horaciocoronel <arielc@gmail.com>
2018-05-24 13:27:16 -0400

    Create contact subComponent

---
+++ client/src/components/bottom/_Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import github from '../../assets/images/github.png';
+
+const Contact = (props) => {
+    return (
+      <div className="bottom">
+        <div className="created-by">
+          Created by <a href="#">Horacio Coronel</a> | <a href="https://github.com/horaciocoronel" title="Horacio Coronel | Github"><img src={github} className="contact-icon" /> Github</a>
+        </div>
+      </div> 
+    )
+}
+
+export default Contact;
