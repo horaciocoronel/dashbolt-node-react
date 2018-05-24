bcc271c5f5e39110de96d434bb0c6f02149cd07b
horaciocoronel <arielc@gmail.com>
2018-05-24 10:52:03 -0400

    Api calls to get weather

---
+++ client/src/utils/Weather.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const LOCATION_URL = "http://ip-api.com/json";
+const DARK_SKY_URL = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY}`;
+import axios from 'axios';
+export {getWeather};
+
+async function getLocation() {
+  const url = `${LOCATION_URL}`;
+  let location;  
+  return axios.get(url)
+    .then(result => {
+      location = {
+        lat: result.data.lat,
+        lon: result.data.lon
+      }
+      return location
+    })
+}
+
+async function getWeatherApi(location) {
+  const url = `${DARK_SKY_URL}/${location.lat},${location.lon}`;
+  return axios.get(url)
+    .then(res => res.data.currently)
+}
+
+
+async function getWeather() {
+  let location = await getLocation();
+  return await getWeatherApi(location);
+}
+
+getWeather()
