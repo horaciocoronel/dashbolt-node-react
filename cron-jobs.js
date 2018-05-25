7f6be1cc5fc5bb5e8d57eaba4f581bdae2b2e8c9
horaciocoronel <arielc@gmail.com>
2018-05-25 10:26:33 -0400

    Add 7pm to cron job

--- cron-jobs.js
+++ cron-jobs.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 const getPictures = require('./services/unsplash');
 
-cron.schedule('0 0,4,6,12,18,19 * * 0-6', () => {
+cron.schedule('0 0,4,6,7,12,18,19 * * 0-6', () => {
   getPictures;
   console.log(`Run at ${Date.now()}`)
 });
