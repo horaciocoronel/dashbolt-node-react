564edb8bb1c4cae51c940720075aba704d4cc824
horaciocoronel <arielc@gmail.com>
2018-05-25 10:51:43 -0400

    Add linkedin url

--- client/src/components/bottom/_Contact.js
+++ client/src/components/bottom/_Contact.js
@@ -5,7 +5,7 @@ const Contact = (props) => {
     return (
       <div className="bottom">
         <div className="created-by">
-          Created by <a href="#">Horacio Coronel</a> | <a href="https://github.com/horaciocoronel" title="Horacio Coronel | Github"><img src={github} className="contact-icon" /> Github</a>
+          Created by <a href="https://www.linkedin.com/in/horacio-coronel/">Horacio Coronel</a> | <a href="https://github.com/horaciocoronel" title="Horacio Coronel | Github"><img src={github} className="contact-icon" /> Github</a>
         </div>
       </div> 
     )
