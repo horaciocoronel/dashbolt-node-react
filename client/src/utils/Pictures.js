6acd46a69e6d8c1486e9eb7a6c90f210c0f59b11
horaciocoronel <arielc@gmail.com>
2018-05-25 10:49:40 -0400

    Add Filter by time of day.

--- client/src/utils/Pictures.js
+++ client/src/utils/Pictures.js
@@ -8,8 +8,14 @@ function getPictures() {
     .then(res => res.json())
     .then(
       result => {
+        
         let pictureIndex = Math.floor((Math.random() * 30) + 1);
-        let picture = (result[pictureIndex-1])
+        let timeOfDayPic = result.filter(picture => {
+          if (picture.timeofday === timeOfDay()) {
+            return picture
+          }
+        })
+        let picture = (timeOfDayPic[pictureIndex-1])
         return picture
       }
     )
@@ -20,4 +26,33 @@ function getPictures() {
           }
       }
   })
+}
+
+
+const timeOfDay = () => {
+  let currentTime = new Date();
+  let currentHours = currentTime.getHours();
+  
+  switch(true) {
+    case currentHours <= 4:
+      return "city%20night"
+        break;
+    case currentHours <= 6:
+      return "sunrise"
+        break;
+    case currentHours <= 12:
+      return "city%20morning"
+        break;
+    case currentHours <= 18:
+      return "sunny%20afternoon"
+        break;
+    case currentHours <= 19:
+      return "sunset"
+        break;
+    case currentHours <= 24:
+      return "city%20night"
+        break;
+    default:
+      return "landscape%20city"
+  }
 }
